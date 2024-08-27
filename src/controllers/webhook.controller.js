14ae5a77275a3688a2e5a688a4b94dfb0d091329
ngarcia-dev <garcianahuel77@gmail.com>
2024-08-27 12:18:34 -0300

    add templates json

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,4 +1,39 @@
 import twilio from "twilio";
+import universityData from "../../templates_ffyb.json" with { type: "json" };
+
+const {
+  welcome,
+  info_alumnos,
+  carreras,
+  info_carreras,
+  plan_estudio,
+  video_farm,
+  que_hace_un,
+} = universityData.content_template;
+
+const {
+  im_study_id,
+  info_cbc_id,
+  study_ffyb_id
+} = welcome.weolcome_item_ids;
+
+const {
+  becas,
+  cursada,
+  idiomas_y_deportes,
+  pasantias,
+  regularidad,
+  priority_inscription,
+} = info_alumnos.info_alumnos_item_ids;
+
+const {
+  farmacia,
+  bioquimica,
+  lcta,
+  tumn,
+  tuoc,
+  tugib
+} = carreras.carreras_item_ids;
 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, MESSAGING_SERVICE_SID } =
   process.env;
@@ -6,12 +41,21 @@ const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, MESSAGING_SERVICE_SID } =
 export const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 export const webhook = async (req, res) => {
-  const { ProfileName, From, Body } = req.body;
+  const { ProfileName, From, Body: message } = req.body;
   const twiml = new twilio.twiml.MessagingResponse();
 
-  if (Body === "Hola") {
+  if (message === "Hola") {
     await client.messages.create({
-      contentSid: "HX310cec6816b9031e8dbdbc20dc78e766",
+      contentSid: welcome._sid,
+      contentVariables: JSON.stringify({ 1: ProfileName }),
+      messagingServiceSid: MESSAGING_SERVICE_SID,
+      to: From,
+    });
+  }
+
+  if (message === im_study_id) {
+    await client.messages.create({
+      contentSid: info_alumnos._sid,
       messagingServiceSid: MESSAGING_SERVICE_SID,
       to: From,
     });
